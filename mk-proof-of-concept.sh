ab472813cb1cb75e8232fbe48b51459132c1d890
R. S. Doiel <rsdoiel@gmail.com>
2016-05-03 14:33:29 -0700

    Build a proof of concept

--- mk-proof-of-concept.sh
+++ mk-proof-of-concept.sh
@@ -5,14 +5,21 @@ cp -v $HOME/src/github.com/rsdoiel/micro/cmd/micro/*.go ./
 
 echo "Renaming package main to package minie"
 ls -1 ./*.go | while read ITEM; do
-   sed -e "/package main/package minie/" $ITEM
+    mv $ITEM $ITEM.bak
+   sed -e 's/package main/package minie/g' $ITEM.bak > $ITEM
 done
 
 echo "Adjusting configuration path in micro.go"
+if [ -f "micro.go" ]; then
+    /bin/mv micro.go micro.go.bak
+    sed -e 's/configDir = xdgHome + "\/micro"/configDir = path.Join(hdgHome, path.Base(os.Args[0]))/g' micro.go.bak > micro.go
+    goimport -w micro.go
+fi
+
 
 echo "Creating wrapper of micro clone"
 mkdir -p cmds/micro
-echo <<CODE
+cat <<CODE > cmds/micro/micro.go
 package main
 
 import "github.com/rsdoiel/minie"
@@ -22,4 +29,6 @@ func main() {
 }
 CODE
 
+echo "Removing up .bak file"
+/bin/rm *.bak
 
